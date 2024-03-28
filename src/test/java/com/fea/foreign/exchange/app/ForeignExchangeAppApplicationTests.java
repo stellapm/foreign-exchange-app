89f3d1bfe4860f75d23ec74e80add723d15209d2
stellapm <stellapm@hotmail.com>
2024-03-28 17:58:28 +0200

    Project setup, initial commit

---
+++ src/test/java/com/fea/foreign/exchange/app/ForeignExchangeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fea.foreign.exchange.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignExchangeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
